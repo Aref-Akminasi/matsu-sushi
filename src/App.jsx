60ee2c5ea3e86155a048567010dd591491ce75b7
Aref Akminasi <137000185+Aref-Akminasi@users.noreply.github.com>
2023-10-23 17:56:19 +0200

    Added content

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,14 @@
 import { useState } from 'react';
 import reactLogo from './assets/react.svg';
 import viteLogo from '/vite.svg';
-import './App.css';
+import classes from './App.module.css';
 
 function App() {
   const [count, setCount] = useState(0);
 
   return (
     <>
-      <h1>Hello world</h1>
+      <h1 className={classes.header}>Hello world</h1>
     </>
   );
 }
