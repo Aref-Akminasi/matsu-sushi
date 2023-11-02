0b6c26f4f6651a19d8d008acd4fa63078bbf33f3
Aref Akminasi <137000185+Aref-Akminasi@users.noreply.github.com>
2023-11-02 10:54:35 +0100

    added cart animation

--- src/components/cart/Cart.jsx
+++ src/components/cart/Cart.jsx
@@ -1,18 +1,27 @@
 import CartIcon from './CartIcon';
 import styles from './Cart.module.css';
-import { useContext } from 'react';
+import { useContext, useEffect, useState } from 'react';
 import CartContext from '../../../cart-context/CartContext';
 
 const Cart = () => {
   const { basketContents } = useContext(CartContext);
   const { setBasketIsDisplayed } = useContext(CartContext);
+  const [active, setActive] = useState(false);
 
   const basketDisplayHandler = () => {
     setBasketIsDisplayed(true);
   };
 
+  useEffect(() => {
+    setActive(true);
+    setTimeout(() => setActive(false), 300);
+  }, [basketContents]);
+
   return (
-    <div className={styles.cart} onClick={basketDisplayHandler}>
+    <div
+      className={`${styles.cart} ${active && styles.active}`}
+      onClick={basketDisplayHandler}
+    >
       <CartIcon />
       <span>{basketContents.length}</span>
     </div>
